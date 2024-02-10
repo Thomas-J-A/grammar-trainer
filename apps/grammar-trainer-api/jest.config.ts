fd6910fe3afd63f720e283a6f20a6c7243a00bd3
Thomas Aspbury <tomaspbury@icloud.com>
2024-02-10 18:06:49 +0000

    Initial commit

---
+++ apps/grammar-trainer-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'grammar-trainer-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/grammar-trainer-api',
+};
