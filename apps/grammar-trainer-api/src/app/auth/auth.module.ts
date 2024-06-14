c74dce33f5ead0c30e0e8c08683379dd7aed720e
Thomas Aspbury <tomaspbury@icloud.com>
2024-06-14 14:23:25 +0100

    feat(auth): add validation to login route

--- apps/grammar-trainer-api/src/app/auth/auth.module.ts
+++ apps/grammar-trainer-api/src/app/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { PasswordResetTokensModule } from '../password-reset-tokens/password-reset-tokens.module';
@@ -7,6 +7,7 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { SessionSerializer } from './session.serializer';
 import { AuthController } from './auth.controller';
+import { LoginValidationMiddleware } from './middleware/login-validation.middleware';
 
 @Module({
   imports: [
@@ -18,4 +19,11 @@ import { AuthController } from './auth.controller';
   providers: [AuthService, LocalStrategy, SessionSerializer],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  /**
+   * Apply request data validation middleware to login route
+   */
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('auth/login');
+  }
+}
